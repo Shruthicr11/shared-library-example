e9bdb104f07d5ce99dcd369747727882791b88d4
CR <shruthi.cr@harman.com>
2023-07-24 18:17:24 +0530

    testing lib resource

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "com/shruthi/scripts/${config.name}"    
+  def scriptcontents = libraryResource "com/shruthi/scripts/${config.name}"  
+  sh "echo ${WORKSPACE}"  
   writeFile file: "${config.name}", text: scriptcontents 
   sh "chmod a+x ./${config.name}"
 } 
